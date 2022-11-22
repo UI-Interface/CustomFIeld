0b9bf8ad75b77759d6f58cf033dc8d2abcc7826e
Interface <118592575+UI-Interface@users.noreply.github.com>
2022-11-22 18:05:45 -0500

    Update RayField.lua

--- RayField.lua
+++ RayField.lua
@@ -1031,7 +1031,7 @@ function RayfieldLibrary:CreateWindow(Settings)
 		if not Passthrough then
 			local AttemptsRemaining = math.random(2,6)
 			Rayfield.Enabled = false
-			local KeyUI = script.Parent:WaitForChild('Key')
+			local KeyUI = game:GetObjects("rbxassetid://11380036235")[1]
 
 			if gethui then
 				KeyUI.Parent = gethui()
